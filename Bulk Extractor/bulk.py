2a4209cc8da2ef90f949f8641d8e31af5ab9641f
Rishabh Jain <54959830+Rishabh-Jain21@users.noreply.github.com>
2020-08-08 10:25:42 +0530

    first commit

--- Bulk Extractor/bulk.py
+++ Bulk Extractor/bulk.py
@@ -1,1 +1,38 @@
 
+import os
+import shutil
+import time
+from datetime import datetime
+
+
+'''
+Put this python file in another directorty or result may cause problem
+'''
+
+
+def bulk_extractor(folder_path):
+    """
+    Move all files in all subfolders to a single folder
+    """
+    if os.getcwd() == folder_path:
+        print("folder path invalid")
+    else:
+        os.chdir(folder_path)
+        f_path = os.path.join(folder_path, "Extracted")
+        if not os.path.exists(f_path):
+            os.makedirs(f_path)
+
+        for dirpath, dirname, filenames in os.walk(folder_path):
+            if dirname != 'Extracted':
+                for f in filenames:
+                    fp = os.path.join(dirpath, f)
+                    if os.path.isfile(fp):
+                        try:
+                            source = os.path.join(folder_path, fp)
+                            destination = f_path
+                            test = shutil.move(source, destination)
+                        except:
+                            pass
+
+
+bulk_extractor("#Enter file path")
