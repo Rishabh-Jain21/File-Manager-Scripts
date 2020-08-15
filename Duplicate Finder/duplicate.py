bdef0749f046a07a470ab7e6ff17e9e6a50d155d
Rishabh Jain <54959830+Rishabh-Jain21@users.noreply.github.com>
2020-08-15 08:20:23 +0530

    Update duplicate.py

--- Duplicate Finder/duplicate.py
+++ Duplicate Finder/duplicate.py
@@ -1,22 +1,38 @@
 import os
 from hashlib import md5
-start_path = r"#Enter the starting directory"
+from threading import Thread
+import time
+
+c1 = time.time()
+
+
+def checker(dirpath, duplicates, file_name):
+    fp = os.path.join(dirpath, file_name)
+    if os.path.isfile(fp):
+        try:
+            fi = open(fp, 'rb')
+            hexa_code = md5(fi.read()).hexdigest()
+            duplicates[hexa_code] = duplicates.get(
+                hexa_code, [])+[os.path.abspath(fp)]
+        except:
+            pass
+
+
+start_path = r"E:\Screenshot"
 duplicates = dict()
+
 for dirpath, dirname, filenames in os.walk(start_path):
     for f in filenames:
-        fp = os.path.join(dirpath, f)
-        if os.path.isfile(fp):
-            try:
-                fi = open(fp, 'rb')
-                hexa_code = md5(fi.read()).hexdigest()
-                duplicates[hexa_code] = duplicates.get(
-                    hexa_code, [])+[os.path.abspath(fp)]
-            except:
-                pass
+        t = Thread(target=checker, args=(dirpath, duplicates, f))
+        t.start()
+        #checker(dirpath, duplicates, f)
+
 stats = False
-for value in duplicates.values():
-    if len(value) > 1:
+for value in list(duplicates):
+    if len(duplicates[value]) > 1:
         stats = True
-        print(value)
+        print(duplicates[value])
 if not stats:
     print("No duplicates found")
+
+print(time.time()-c1)
